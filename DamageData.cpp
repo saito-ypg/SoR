88eee2c4563e4bbbc08c34e79ab39f05c3210ce5
友輝 斎藤 <220188@jc-21.jp>
2023-12-14 13:41:00 +0900

    つまりかけてきた ダメージデータの構造体作成

--- DamageData.cpp
+++
@@ -1,1 +0,0 @@
-#include "DamageData.h"
--- DamageData.h
+++ DamageData.h
@@ -1,5 +1,10 @@
 #pragma once
-class DamageData
+#include<DirectXMath.h>
+//ダメージを受ける際に必要な情報を格納
+struct DamageData
 {
+	float damage_;//ダメージ量
+	float direction_;//攻撃の方向、ノックバックする向き(y軸回転、deg)
+	float knockback_;//ノックバック強さ
 };
 
