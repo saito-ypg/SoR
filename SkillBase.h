7c10d83dd14c0484f2032692c350efd585cfe11d
友輝 斎藤 <220188@jc-21.jp>
2023-12-19 15:09:27 +0900

    スキルの実装方法が一生定まらない

--- SkillBase.h
+++ SkillBase.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "AttackRange.h"
 #include"DamageData.h"
-//プレイヤー用スキルの
+//プレイヤー用スキルの元
 class SkillBase
 {
+public:
 	struct DamageData damageData_;
 
 	const float defaultCastTime_;//スキル使用にかかる総時間
@@ -12,8 +13,8 @@ class SkillBase
 	float coolDown_;//クールタイム残り時間
 
 	SkillBase(float CT,float CD):defaultCastTime_(CT),defaultCoolDown_(CD){}
-	virtual void action() = 0;
-		void update();
+	virtual void action() = 0;//スキル発動した時の動きとかを実装
+	void update();
 
 };
 
