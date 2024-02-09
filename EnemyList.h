376d9146465d61a87ae793afc95957b93d9b7ba5
友輝 斎藤 <220188@jc-21.jp>
2024-02-09 13:41:40 +0900

    とりあえずEnemyListクラス作成 スポーンさせるクラスは別に作る

--- EnemyList.h
+++ EnemyList.h
@@ -1,11 +1,20 @@
 #pragma once
 #include<list>
 #include"EnemyList.h"
+#include"GameActor.h"
 class EnemyBase;
 //敵をまとめて管理するクラス
-//現段階では未使用
-namespace Enemy
-{
+//スポーンさせるクラスが別にいてほしいな～
+class EnemyList:public GameObject{
+private:
+
+public:
+	EnemyList(GameObject* parent);
+	~EnemyList();
+	void Initialize()override;
+	void Update()override;
+	void Draw()override;
+	void Release()override;
 	std::list<EnemyBase*> Enemylist;
 
 	void add(EnemyBase*enemy);//listに敵を登録
