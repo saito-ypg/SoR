8a21df57412aa13a5923c94705c41d211f6e0c45
友輝 斎藤 <220188@jc-21.jp>
2024-06-12 16:33:38 +0900

    Easing修正+AI準備

--- sources/EnemyBase.h
+++ sources/EnemyBase.h
@@ -2,7 +2,7 @@
 #include "GameActor.h"
 #include"EnemyType.h"
 struct EnemyStatus {
-	int hp;
+	float hp;
 	float radius;
 };
 
@@ -10,7 +10,7 @@ class EnemyBase : public GameActor
 {
 	
 public:
-	enum STATE { WAIT, IN_SPAWN, DEAD };
+	enum SPAWINIG_STATE { WAIT, IN_SPAWN, DEAD };
 private:
 	
 protected:
@@ -20,7 +20,7 @@ protected:
 
 	//スキル等を知る必要ないためPlayer型にはしない
 	GameActor* pPlayer;
-	STATE eStat_;//管理用
+	SPAWINIG_STATE eStat_;//管理用
 public:
 	//EnemyBase(GameObject* parent);
 	EnemyBase(GameObject* parent, EnemyType type, bool isboss = false);
@@ -28,7 +28,7 @@ public:
 
 	void SetPlayer(GameActor* p) { pPlayer = p; assert(pPlayer != nullptr); }
 	void setConfig(EnemyStatus status);
-	STATE getStat() const { return eStat_; }
+	SPAWINIG_STATE getStat() const { return eStat_; }
 	
 	
 	
